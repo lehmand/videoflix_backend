3a9882fc8422ea91ec201ef744402dbc2c6c03b4
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-04-10 21:49:19 +0200

    added staticfolder

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'videos.apps.VideosConfig',
     'debug_toolbar',
     'django_rq',
+    'import_export',
 ]
 
 MIDDLEWARE = [
@@ -161,6 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- videos/admin.py
+++ videos/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Video
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 # Register your models here.
 
-admin.site.register(Video)
+class VideoResources(resources.ModelResource):
+
+    class Meta:
+        model = Video
+
+@admin.register(Video)
+class VideoAdmin(ImportExportModelAdmin):
+    resource_classes = [VideoResources]
