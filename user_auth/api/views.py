78bc0283a1b563845cf0a0d19a7ec19ed92a37a4
Daniel Lehmann <daniel.lehmann188@gmail.com>
2025-04-30 19:12:59 +0200

    changed resetpassword link

--- user_auth/api/views.py
+++ user_auth/api/views.py
@@ -97,7 +97,7 @@ class PasswordResetRequestView(APIView):
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         token = default_token_generator.make_token(user)
         
-        reset_link = f"http://vid.daniel-lehmann.dev/reset/{uid}/{token}/"
+        reset_link = f"http://vid.daniel-lehmann.dev/api/auth/reset/{uid}/{token}/"
         
         send_reset_mail(user.email, user.username, reset_link)
         
